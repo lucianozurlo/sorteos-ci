179dd1c9581b7a53eb1b19a6388f75e0233cfb31
Luciano Zurlo <luciano@aiMac.local>
2025-02-03 09:47:56 -0300

    all

--- sorteo_project/sorteo-frontend/src/config.js
+++ sorteo_project/sorteo-frontend/src/config.js
@@ -3,4 +3,5 @@
 //   process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000';
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://sorteos-ci.up.railway.app';
+  process.env.REACT_APP_API_BASE_URL ||
+  'https://sorteos-ci-production.up.railway.app/';
